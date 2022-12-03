5ab5ba3bdad641285259377396aa0603163b2952
Леонид Бельков <leonid-kb@outlook.com>
2022-12-03 14:06:47 +0500

    задание 2

---
+++ IS-2-20-BelkolLK-U/TK2.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace IS_2_20_BelkolLK_U
+{
+    partial class Form2
+    {/// <summary>
+     /// Обязательная переменная конструктора.
+     /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 29);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "подключение к БД";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 82);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ IS-2-20-BelkolLK-U/TK2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace IS_2_20_BelkolLK_U
+{
+    public partial class Form2 : Form
+    {
+
+        public class BDconnect
+        {
+            protected string connect;
+
+            public BDconnect(string Connect)
+            {
+                this.connect = Connect;
+            }
+
+            public string GetConnectionString()
+            {
+                return this.connect;
+            }
+        }
+
+        BDconnect BD = new BDconnect("server=chuc.caseum.ru;port=33333;user=uchebka;" +
+                "database=uchebka;password=uchebka;");
+
+        SqlConnection con;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(BD.GetConnectionString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                MessageBox.Show("Соединение работает");
+            }
+            catch
+            {
+                MessageBox.Show("Соединение не работает");
+            }
+            finally
+            {              
+                con.Close();
+            }
+        }
+
+}    }
